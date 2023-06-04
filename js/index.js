af62eae389c4d2bc312338f39a457175485927d9
Maria Victoria Gonzalez <mvg915@hotmail.com>
2023-06-04 19:56:57 -0300

    agregando detalles html y css

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ class producto {
         this.categoria = categoria
     }
 }
-const nuevoProducto = new producto ("Manga Kimetsu no Yaiba", "m0223", 2200, "mangas")
+const nuevoProducto = new producto("Manga Kimetsu no Yaiba", "m0223", 2200, "mangas")
 
 const productos = [
     { nombre: "Figura Hitsune Miku:", codigo: "f0234", precio: 2990, categoria: "figuras" },
@@ -28,23 +28,21 @@ function procesarCompra() {
         llenarCarrito()
         respuestaCompra = confirm("Desea agregar otro producto al carrito?")
     } while (respuestaCompra);
-
     cerrarCompra()
 }
 
 function llenarCarrito() {
     let eleccion = prompt("Productos disponibles: Figura Hitsune Miku: codigo: f0234, precio: $2990 | Manga Jujutsu Kaisen: codigo: m1991, precio: $2000 | Figura Naruto Shipuden: codigo: f5836, precio: $2750 | Manga Kimetsu no Yaiba: codigo m0223, precio: $2200. Ingresa el codigo del producto.")
-        let productoElegido = productos.find((producto) => producto.codigo === eleccion)
-        if (productoElegido !== undefined) {
-            productosElegidos.push(productoElegido)
-        }else {
-            alert("Codigo incorrecto! Intenta de nuevo.")
-        }
+    let productoElegido = productos.find((producto) => producto.codigo === eleccion)
+    if (productoElegido !== undefined) {
+        productosElegidos.push(productoElegido)
+    } else {
+        alert("Codigo incorrecto! Intenta de nuevo.")
+    }
 }
 
 function cerrarCompra() {
     const totalCompra = productosElegidos.reduce((acc, productoElegido) => acc + productoElegido.precio, 0)
-
     console.log("Los productos elegidos son: ")
     console.table(productosElegidos)
     console.log("El total de su compra es: $" + totalCompra)
