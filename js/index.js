1b69f175cecbf9b487c7d182937b71185264dc87
Maria Victoria Gonzalez <mvg915@hotmail.com>
2023-06-04 19:47:40 -0300

    agregando clase constructora

--- js/index.js
+++ js/index.js
@@ -4,11 +4,22 @@ function saludar() {
 
 saludar()
 
+class producto {
+    constructor(nombre, codigo, precio, categoria) {
+        this.nombre = nombre
+        this.codigo = codigo
+        this.precio = precio
+        this.categoria = categoria
+    }
+}
+const nuevoProducto = new producto ("Manga Kimetsu no Yaiba", "m0223", 2200, "mangas")
+
 const productos = [
     { nombre: "Figura Hitsune Miku:", codigo: "f0234", precio: 2990, categoria: "figuras" },
     { nombre: "Manga Jujutsu Kaisen:", codigo: "m1991", precio: 2000, categoria: "mangas" },
     { nombre: "Figura Naruto Shipuden:", codigo: "f5836", precio: 2750, categoria: "figuras" }
 ]
+productos.push(nuevoProducto)
 
 let productosElegidos = []
 procesarCompra()
@@ -22,7 +33,7 @@ function procesarCompra() {
 }
 
 function llenarCarrito() {
-    let eleccion = prompt("Productos disponibles: Figura Hitsune Miku: codigo: f0234, precio: $2990 | Manga Jujutsu Kaisen: codigo: m1991, precio: 2000, Figura Naruto Shipuden: codigo: f5836, precio: 2750. Ingresa el codigo del producto.")
+    let eleccion = prompt("Productos disponibles: Figura Hitsune Miku: codigo: f0234, precio: $2990 | Manga Jujutsu Kaisen: codigo: m1991, precio: $2000 | Figura Naruto Shipuden: codigo: f5836, precio: $2750 | Manga Kimetsu no Yaiba: codigo m0223, precio: $2200. Ingresa el codigo del producto.")
         let productoElegido = productos.find((producto) => producto.codigo === eleccion)
         if (productoElegido !== undefined) {
             productosElegidos.push(productoElegido)
@@ -30,7 +41,6 @@ function llenarCarrito() {
             alert("Codigo incorrecto! Intenta de nuevo.")
         }
 }
-
 
 function cerrarCompra() {
     const totalCompra = productosElegidos.reduce((acc, productoElegido) => acc + productoElegido.precio, 0)
@@ -40,4 +50,3 @@ function cerrarCompra() {
     console.log("El total de su compra es: $" + totalCompra)
     console.log("Gracias por tu compra!")
 }
-
